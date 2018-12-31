ec00a9001be823fc34cc93ad8b9b02c21018c06d
stasyamir <stanislav.miroshnyk@nure.ua>
2018-12-31 17:04:19 +0200

    drop template files

--- agent/src/main/java/ua/nure/kn/miroshnik/usermanagment/agent/exception/SearchException.java
+++
@@ -1,9 +0,0 @@
-package ua.nure.kn.miroshnik.usermanagment.agent.exception;
-
-public class SearchException extends Exception {
-    private String name;
-
-    public SearchException(Exception e) {
-        this.name = e.toString();
-    }
-}
